2f14fd2fba33f116bd2b3aafec5b01a1bc822c55
Zachary Conley <conley_zachary@ad1.columbusstate.edu>
2023-02-21 19:10:24 -0500

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,164 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Lblinstructions = New System.Windows.Forms.Label()
+        Me.Lblinstructions2 = New System.Windows.Forms.Label()
+        Me.Btnnum1 = New System.Windows.Forms.Button()
+        Me.Btnnum2 = New System.Windows.Forms.Button()
+        Me.Btnnum3 = New System.Windows.Forms.Button()
+        Me.Btnnum4 = New System.Windows.Forms.Button()
+        Me.Btnnum5 = New System.Windows.Forms.Button()
+        Me.Btnexit = New System.Windows.Forms.Button()
+        Me.Lblfrench = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Lblinstructions
+        '
+        Me.Lblinstructions.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Lblinstructions.Location = New System.Drawing.Point(219, 9)
+        Me.Lblinstructions.Name = "Lblinstructions"
+        Me.Lblinstructions.Size = New System.Drawing.Size(391, 64)
+        Me.Lblinstructions.TabIndex = 0
+        Me.Lblinstructions.Text = "Do you know the french words for the number 1 through 5?"
+        Me.Lblinstructions.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Lblinstructions2
+        '
+        Me.Lblinstructions2.AutoSize = True
+        Me.Lblinstructions2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Lblinstructions2.Location = New System.Drawing.Point(240, 73)
+        Me.Lblinstructions2.Name = "Lblinstructions2"
+        Me.Lblinstructions2.Size = New System.Drawing.Size(352, 25)
+        Me.Lblinstructions2.TabIndex = 1
+        Me.Lblinstructions2.Text = "Click the buttons below to see them"
+        '
+        'Btnnum1
+        '
+        Me.Btnnum1.BackColor = System.Drawing.Color.Silver
+        Me.Btnnum1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Btnnum1.ForeColor = System.Drawing.Color.Black
+        Me.Btnnum1.Location = New System.Drawing.Point(143, 162)
+        Me.Btnnum1.Name = "Btnnum1"
+        Me.Btnnum1.Size = New System.Drawing.Size(76, 33)
+        Me.Btnnum1.TabIndex = 2
+        Me.Btnnum1.Text = "1"
+        Me.Btnnum1.UseVisualStyleBackColor = False
+        '
+        'Btnnum2
+        '
+        Me.Btnnum2.BackColor = System.Drawing.Color.Silver
+        Me.Btnnum2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Btnnum2.Location = New System.Drawing.Point(260, 162)
+        Me.Btnnum2.Name = "Btnnum2"
+        Me.Btnnum2.Size = New System.Drawing.Size(76, 33)
+        Me.Btnnum2.TabIndex = 3
+        Me.Btnnum2.Text = "2"
+        Me.Btnnum2.UseVisualStyleBackColor = False
+        '
+        'Btnnum3
+        '
+        Me.Btnnum3.BackColor = System.Drawing.Color.Silver
+        Me.Btnnum3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Btnnum3.Location = New System.Drawing.Point(367, 162)
+        Me.Btnnum3.Name = "Btnnum3"
+        Me.Btnnum3.Size = New System.Drawing.Size(76, 33)
+        Me.Btnnum3.TabIndex = 4
+        Me.Btnnum3.Text = "3"
+        Me.Btnnum3.UseVisualStyleBackColor = False
+        '
+        'Btnnum4
+        '
+        Me.Btnnum4.BackColor = System.Drawing.Color.Silver
+        Me.Btnnum4.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Btnnum4.Location = New System.Drawing.Point(483, 162)
+        Me.Btnnum4.Name = "Btnnum4"
+        Me.Btnnum4.Size = New System.Drawing.Size(76, 33)
+        Me.Btnnum4.TabIndex = 5
+        Me.Btnnum4.Text = "4"
+        Me.Btnnum4.UseVisualStyleBackColor = False
+        '
+        'Btnnum5
+        '
+        Me.Btnnum5.BackColor = System.Drawing.Color.Silver
+        Me.Btnnum5.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Btnnum5.Location = New System.Drawing.Point(600, 162)
+        Me.Btnnum5.Name = "Btnnum5"
+        Me.Btnnum5.Size = New System.Drawing.Size(76, 33)
+        Me.Btnnum5.TabIndex = 6
+        Me.Btnnum5.Text = "5"
+        Me.Btnnum5.UseVisualStyleBackColor = False
+        '
+        'Btnexit
+        '
+        Me.Btnexit.BackColor = System.Drawing.Color.Silver
+        Me.Btnexit.Location = New System.Drawing.Point(367, 327)
+        Me.Btnexit.Name = "Btnexit"
+        Me.Btnexit.Size = New System.Drawing.Size(76, 33)
+        Me.Btnexit.TabIndex = 7
+        Me.Btnexit.Text = "Exit"
+        Me.Btnexit.UseVisualStyleBackColor = False
+        '
+        'Lblfrench
+        '
+        Me.Lblfrench.BackColor = System.Drawing.Color.Gainsboro
+        Me.Lblfrench.ForeColor = System.Drawing.Color.Red
+        Me.Lblfrench.Location = New System.Drawing.Point(367, 247)
+        Me.Lblfrench.Name = "Lblfrench"
+        Me.Lblfrench.Size = New System.Drawing.Size(76, 33)
+        Me.Lblfrench.TabIndex = 8
+        Me.Lblfrench.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Gainsboro
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Lblfrench)
+        Me.Controls.Add(Me.Btnexit)
+        Me.Controls.Add(Me.Btnnum5)
+        Me.Controls.Add(Me.Btnnum4)
+        Me.Controls.Add(Me.Btnnum3)
+        Me.Controls.Add(Me.Btnnum2)
+        Me.Controls.Add(Me.Btnnum1)
+        Me.Controls.Add(Me.Lblinstructions2)
+        Me.Controls.Add(Me.Lblinstructions)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "French Numbers"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Lblinstructions As Label
+    Friend WithEvents Lblinstructions2 As Label
+    Friend WithEvents Btnnum1 As Button
+    Friend WithEvents Btnnum2 As Button
+    Friend WithEvents Btnnum3 As Button
+    Friend WithEvents Btnnum4 As Button
+    Friend WithEvents Btnnum5 As Button
+    Friend WithEvents Btnexit As Button
+    Friend WithEvents Lblfrench As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,42 @@
+﻿' Project name: French numbers
+' Author: Zachary Conley
+' Date: February 21,2023
+' Purpose: This application is used to show the numbers 1 through 5 in the french language when the particular button is clicked
+Public Class Form1
+    Private Sub Btnexit_Click(sender As Object, e As EventArgs) Handles Btnexit.Click
+        'This event handler closes the application once clicked
+        Close()
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MinimizeBox = False
+        MaximizeBox = False
+
+    End Sub
+
+    Private Sub Btnnum1_Click(sender As Object, e As EventArgs) Handles Btnnum1.Click
+        Lblfrench.Text = "Un"
+
+    End Sub
+
+    Private Sub Btnnum2_Click(sender As Object, e As EventArgs) Handles Btnnum2.Click
+        Lblfrench.Text = "Deux"
+
+    End Sub
+
+    Private Sub Btnnum3_Click(sender As Object, e As EventArgs) Handles Btnnum3.Click
+        Lblfrench.Text = "Trois"
+
+    End Sub
+
+    Private Sub Btnnum4_Click(sender As Object, e As EventArgs) Handles Btnnum4.Click
+        Lblfrench.Text = "Quatre"
+
+    End Sub
+
+    Private Sub Btnnum5_Click(sender As Object, e As EventArgs) Handles Btnnum5.Click
+        Lblfrench.Text = "Cinq"
+
+    End Sub
+End Class
